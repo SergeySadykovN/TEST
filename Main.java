40cae6805bd1e656d8e1a0e1d2d58b5c29b5766f
Sergey Sadykov <snsadykov@ya.ru>
2023-02-02 14:50:21 +0300

    create Scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+                System.out.println("Type Name :");
+                String a = scn.next();
+                System.out.println("Type age");
+                int b = scn.nextInt();
+                System.out.println("Your name is " + a + " "+ "Your age is " + b);
 
-    }
+// 1. a= b= good, a= b!= ok, a!= b!= no good
+                if (a.equals("Sergey") && b<=40) {
+                    System.out.println("good");
+                } else if (a.equals("Sergey") || b <= 40) {
+                    System.out.println("ok");
+                } else {
+                    System.out.println(" no good");
+                }
+
+
+            }
 }
